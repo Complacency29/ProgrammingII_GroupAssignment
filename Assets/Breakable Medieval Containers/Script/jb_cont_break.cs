d67f1fab68eaf0ecc58290ad202ee7fd2b18bdfc
Complacency29 <cwinstone0352@gmail.com>
2023-09-05 14:17:22 -0500

    imported some props
    Signed-off-by: Complacency29 <cwinstone0352@gmail.com>

---
+++ Assets/Breakable Medieval Containers/Script/jb_cont_break.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jb_cont_break : MonoBehaviour {
+	public Transform Fragments;
+	public float Spread, Force;
+	void OnMouseDown () {
+		Destroy(gameObject);
+		Instantiate(Fragments, transform.position, transform.rotation);
+		Fragments.localScale = transform.localScale;
+		Vector3 breakPosition = transform.position;
+		Collider[] fragments = Physics.OverlapSphere(breakPosition, Spread);
+
+		foreach (Collider fragment in fragments) {
+			if (fragment.attachedRigidbody) {
+				fragment.attachedRigidbody.AddExplosionForce(Force, breakPosition, Spread);
+			}
+		}
+	}
+}

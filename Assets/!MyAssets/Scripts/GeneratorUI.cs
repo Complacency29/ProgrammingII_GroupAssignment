569d67c98e1b1936aba2745cb1ff82cdd9755650
Complacency29 <cwinstone0352@gmail.com>
2023-09-22 20:44:13 -0500

    fixed stack overflow error
    Signed-off-by: Complacency29 <cwinstone0352@gmail.com>

--- Assets/!MyAssets/Scripts/GeneratorUI.cs
+++ Assets/!MyAssets/Scripts/GeneratorUI.cs
@@ -6,7 +6,7 @@ public class GeneratorUI : MonoBehaviour
 {
     private void Start()
     {
-        //ModuleSnapping.Generator.Instance.GenerateModules();
+        ModuleSnapping.Generator.Instance.GenerateModules();
     }
 
     public void GenerateWorldButton()

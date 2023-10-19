c4d7200c1e8134d5bdcb20a1d96ceed2313aa43c
Complacency29 <cwinstone0352@gmail.com>
2023-10-19 18:51:59 -0500

    working on player movement
    Signed-off-by: Complacency29 <cwinstone0352@gmail.com>

--- Assets/!MyAssets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/!MyAssets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -34,11 +34,6 @@ namespace ModuleSnapping
 
             controls = new InputMaster();
             //controls.PlayerMovement.Move.performed += context => PlayerMove(context.ReadValue<Vector2>());
-
-            if (_photonView.IsMine)
-            {
-                animator = GetComponent<Animator>();
-            }
         }
 
         private void Start()
@@ -51,6 +46,9 @@ namespace ModuleSnapping
 
         private void Update()
         {
+            if (_photonView.IsMine == false)
+                return;
+
             isGrounded = Physics.CheckSphere(groundChecker.position, groundCheckDistance, walkableLayers);
             if (isGrounded && velocity.y < 0)
             {
@@ -64,10 +62,23 @@ namespace ModuleSnapping
 
             //Movement gubbins
             Vector2 input = controls.PlayerMovement.Move.ReadValue<Vector2>();
-            PlayerMove(input);
+            _photonView.RPC("PlayerMovementRPC", RpcTarget.All, input);
+            //PlayerMovementRPC(input);
             AnimationController(input);
 
             //Jump gubbins
+            _photonView.RPC("JumpMovementRPC", RpcTarget.All);
+            //JumpMovementRPC();
+        }
+        [PunRPC]
+        void PlayerMovementRPC(Vector2 _input)
+        {
+            Vector3 movement = transform.right * _input.x + transform.forward * _input.y;
+            characterController.Move(movement * baseMoveSpeed * Time.deltaTime);
+        }
+        [PunRPC]
+        void JumpMovementRPC()
+        {
             //if we are pressing the jump button AND we are grounded
             if (controls.PlayerMovement.Jump.ReadValue<float>() > 0 && isGrounded)
             {
@@ -75,16 +86,8 @@ namespace ModuleSnapping
                 //set the y velocity to the amount required to reach the specified jump height based on gravity
                 velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
             }
-
             velocity.y += gravity * Time.deltaTime;
             characterController.Move(velocity * Time.deltaTime);
-        }
-
-        void PlayerMove(Vector2 _input)
-        {
-            Vector3 movement = transform.right * _input.x + transform.forward * _input.y;
-
-            characterController.Move(movement * baseMoveSpeed * Time.deltaTime);
         }
         void AnimationController(Vector2 _input)
         {

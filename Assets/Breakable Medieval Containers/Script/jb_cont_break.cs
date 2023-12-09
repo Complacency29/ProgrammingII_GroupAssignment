a4f997d74c14c888ed5452bd07594adcaf5d4b37
Complacency29 <cwinstone0352@gmail.com>
2023-12-08 18:48:06 -0600

    merging stuff
    Signed-off-by: Complacency29 <cwinstone0352@gmail.com>

--- Assets/Breakable Medieval Containers/Script/jb_cont_break.cs
+++ Assets/Breakable Medieval Containers/Script/jb_cont_break.cs
@@ -1,25 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Photon.Pun;
 
-public class jb_cont_break : MonoBehaviour {
+public class jb_cont_break : MonoBehaviour, IDamageable
+{
 	public Transform Fragments;
 	public float Spread, Force;
 
+    //PhotonView view;
 
-	void OnMouseDown () {
-        GetComponent<LootBag>().InstantiateLoot(transform.position);
+    /*
+    private void Awake()
+    {
+        view = GetComponent<PhotonView>();
+    }
+    */
+    public void Damage(int _amount)
+    {
+        //break the object
+        //BreakObject();
+        //view.RPC("BreakObjectRPC", RpcTarget.All);
+    }
 
+    //[PunRPC]
+    void BreakObjectRPC()
+    {
+        if(GetComponent<LootBag>() != null)
+        {
+            GetComponent<LootBag>().InstantiateLoot(transform.position);
+        }
+
+        Instantiate(Fragments, transform.position, transform.rotation);
+        Fragments.localScale = transform.localScale;
+        Vector3 breakPosition = transform.position;
+        Collider[] fragments = Physics.OverlapSphere(breakPosition, Spread);
+
+        foreach (Collider fragment in fragments)
+        {
+            if (fragment.attachedRigidbody)
+            {
+                fragment.attachedRigidbody.AddExplosionForce(Force, breakPosition, Spread);
+            }
+        }
         Destroy(gameObject);
-		Instantiate(Fragments, transform.position, transform.rotation);
-		Fragments.localScale = transform.localScale;
-		Vector3 breakPosition = transform.position;
-		Collider[] fragments = Physics.OverlapSphere(breakPosition, Spread);
-
-		foreach (Collider fragment in fragments) {
-			if (fragment.attachedRigidbody) {
-				fragment.attachedRigidbody.AddExplosionForce(Force, breakPosition, Spread);
-			}
-		}       
     }
 }

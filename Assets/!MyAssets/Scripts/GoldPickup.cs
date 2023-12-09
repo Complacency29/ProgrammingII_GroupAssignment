4d6b6bd6937af94383e2698358df279ad5f08b44
Complacency29 <cwinstone0352@gmail.com>
2023-12-08 18:16:46 -0600

    Gold pickups complete, added to loot tables
    Signed-off-by: Complacency29 <cwinstone0352@gmail.com>

---
+++ Assets/!MyAssets/Scripts/GoldPickup.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class GoldPickup : MonoBehaviour
+{
+    [SerializeField, Min(0)] int amount;
+    [SerializeField] bool useRandomGoldAmount = false;
+    [SerializeField, Range(1, 1000)] int randomAmountMin = 5;
+    [SerializeField, Range(1, 1000)] int randomAmountMax = 100;
+    [SerializeField, Range(1, 3)] float pickupRange = 1.5f;
+
+    public int Amount { get { return amount; } }
+
+    private void Awake()
+    {
+        GetComponent<SphereCollider>().isTrigger = true;
+        GetComponent<SphereCollider>().radius = pickupRange;
+    }
+    private void Start()
+    {
+        if(useRandomGoldAmount)
+        {
+            int rng = Random.Range(randomAmountMin, randomAmountMax + 1);
+            amount = rng;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+
+        if (inventory != null)
+        {
+            inventory.CurGold += Mathf.Abs(amount);
+            Destroy(gameObject);
+        }
+    }
+}
